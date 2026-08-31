fbd344ac2cfbc6124f174308064dcd191f12a132
kkMihai <kkmihai@proton.me>
2026-08-31 23:21:46 +0300

    chore: release v1.3.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 name 'fivem_conflicttool'
 author 'kkMihai'
-version '1.3.0'
+version '1.3.1'
 description 'in-game map conflict scanner and resolver'
 repository 'https://github.com/kkMihai/fivem_conflicttool'
 license 'GPL-3.0-only'
